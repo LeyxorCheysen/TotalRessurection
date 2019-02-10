e7700e155f307b8d12025fc6f11ec6e848aa7a4b
LeyxorCheysen <38832246+LeyxorCheysen@users.noreply.github.com>
2019-02-10 03:43:15 +0200

    Tests

---
+++ build/sources/main/java/com/leyxorcheysen/totalresurrection/TotalResurrection.java
@@ -0,0 +1,8 @@
+package com.leyxorcheysen.totalresurrection;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = "totalresurrection", name = "Total Resurrection", version = "0.0.0", dependencies = "required-after:forge@[14.23.5.2768,)", useMetadata = true, acceptedMinecraftVersions = "[1.12.2]", modLanguage = "java", updateJSON = "https://raw.githubusercontent.com/LeyxorCheysen/TotalRessurection/master/versions.json")
+public class TotalResurrection {
+
+}
