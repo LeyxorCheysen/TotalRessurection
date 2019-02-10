c5ce51aeadb7af19ff65865fa96989c2be63d413
LeyxorCheysen <38832246+LeyxorCheysen@users.noreply.github.com>
2019-02-10 17:49:51 +0200

    Added blocks init

---
+++ src/main/java/com/leyxorcheysen/totalresurrection/init/BlocksInit.java
@@ -0,0 +1,37 @@
+package com.leyxorcheysen.totalresurrection.init;
+
+import net.minecraft.block.Block;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class BlocksInit {
+	
+	public static void registerBlocks() {
+		
+    }
+
+    @SideOnly(Side.CLIENT)
+    public static void registerRenders() {
+    	
+    }
+
+    private static void setRegister(Block block) {
+    	
+        ForgeRegistries.BLOCKS.register(block);
+        ForgeRegistries.ITEMS.register(new ItemBlock(block).setRegistryName(block.getRegistryName()));
+        
+    }
+
+    @SideOnly(Side.CLIENT)
+    private static void setRender(Block block) {
+    	
+    	Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(Item.getItemFromBlock(block), 0, new ModelResourceLocation(block.getRegistryName(), "inventory"));
+
+    }
+
+}
