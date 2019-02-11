e6cf97cc62b1cba67c903b6716deaccd07f91c3a
LeyxorCheysen <38832246+LeyxorCheysen@users.noreply.github.com>
2019-02-11 13:25:46 +0200

    Tests

--- build/sources/main/java/com/leyxorcheysen/totalresurrection/handlers/EventsHandler.java
+++ build/sources/main/java/com/leyxorcheysen/totalresurrection/handlers/EventsHandler.java
@@ -1,24 +1,53 @@
 package com.leyxorcheysen.totalresurrection.handlers;
 
+import java.util.List;
+
+import com.google.common.collect.ImmutableList;
+
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.world.storage.loot.LootTable;
+import net.minecraft.world.storage.loot.LootEntry;
+import net.minecraft.world.storage.loot.LootEntryTable;
+import net.minecraft.world.storage.loot.LootPool;
 import net.minecraft.world.storage.loot.LootTableList;
+import net.minecraft.world.storage.loot.RandomValueRange;
+import net.minecraft.world.storage.loot.conditions.LootCondition;
 import net.minecraftforge.event.LootTableLoadEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class EventsHandler {
-	
-	@SubscribeEvent
-	public void onLoot(LootTableLoadEvent event) {
+
+private static final List<String> TABLES = ImmutableList.of("life_crystal");
+
+	public EventsHandler() {
 		
-	    if (LootTableList.CHESTS_DESERT_PYRAMID.equals(event.getName())) {
-	    	
-	        ResourceLocation loc = new ResourceLocation("totalresurrection", "chests/total_resurrection_desert_pyramid_chest");
-	        LootTable customLootTable = event.getLootTableManager().getLootTableFromLocation(loc);
-	        event.setTable(customLootTable);
-	        
-	    }
-	    
+		for (String s : TABLES) {
+			
+			LootTableList.register(new ResourceLocation("totalresurrection", s));
+			
+		}
+
 	}
 
+	@SubscribeEvent
+	public void lootLoad(LootTableLoadEvent evt) {
+		String prefix = "minecraft:chests/";
+		String name = evt.getName().toString();
+
+		if (name.startsWith(prefix)) {
+			String file = name.substring(name.indexOf(prefix) + prefix.length());
+			switch (file) {
+			case "desert_pyramid":evt.getTable().addPool(getInjectPool(file)); break;
+			default: break;
+			}
+		}
+	}
+
+	private LootPool getInjectPool(String entryName) {
+		return new LootPool(new LootEntry[] { getInjectEntry(entryName, 1) }, new LootCondition[0], new RandomValueRange(1), new RandomValueRange(0, 1), "totalresurrection");
+	}
+
+	private LootEntryTable getInjectEntry(String name, int weight) {
+		return new LootEntryTable(new ResourceLocation("totalresurrection", "inject/" + name), weight, 0, new LootCondition[0], "totalresurrection");
+	}
+	
 }
