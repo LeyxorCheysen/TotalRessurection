b11e61c43c94f129beefa547bfa64c377f77a91c
LeyxorCheysen <38832246+LeyxorCheysen@users.noreply.github.com>
2019-02-11 10:40:40 +0200

    Added items init

---
+++ src/main/java/com/leyxorcheysen/totalresurrection/init/ItemsInit.java
@@ -0,0 +1,30 @@
+package com.leyxorcheysen.totalresurrection.init;
+
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.common.registry.ForgeRegistries;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class ItemsInit {
+	
+	public static void registerItems() {
+		
+    }
+
+    @SideOnly(Side.CLIENT)
+    public static void registerRenders() {
+    	
+    }
+
+    private static void setRegister(Item item) {
+    	
+        ForgeRegistries.ITEMS.register(item);
+        
+    }
+
+    @SideOnly(Side.CLIENT)
+    private static void setRender(Item item) {
+
+    }
+
+}
