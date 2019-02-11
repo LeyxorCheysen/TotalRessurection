90578b24d4deacaee1378a6739f4d13f2a1c3c2e
LeyxorCheysen <38832246+LeyxorCheysen@users.noreply.github.com>
2019-02-11 11:36:19 +0200

    Changed tool table modify type

--- src/main/java/com/leyxorcheysen/totalresurrection/handlers/EventsHandler.java
+++ src/main/java/com/leyxorcheysen/totalresurrection/handlers/EventsHandler.java
@@ -1,29 +1,21 @@
 package com.leyxorcheysen.totalresurrection.handlers;
 
-import com.leyxorcheysen.totalresurrection.init.ItemsInit;
-
-import net.minecraft.world.storage.loot.LootEntryItem;
-import net.minecraft.world.storage.loot.LootPool;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.storage.loot.LootTable;
 import net.minecraft.world.storage.loot.LootTableList;
-import net.minecraft.world.storage.loot.conditions.LootCondition;
-import net.minecraft.world.storage.loot.functions.LootFunction;
 import net.minecraftforge.event.LootTableLoadEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class EventsHandler {
 	
 	@SubscribeEvent
-	public void onLootTablesLoaded(LootTableLoadEvent event) {
-
-	    if (event.getName().equals(LootTableList.CHESTS_DESERT_PYRAMID)) {
-
-	        final LootPool pool2 = event.getTable().getPool("pool2");
-
-	        if (pool2 != null) {
-
-	            pool2.addEntry(new LootEntryItem(ItemsInit.LIFE_CRYSTAL, 4, 0, new LootFunction[0], new LootCondition[0], "loottable:life_crystal"));
-	        
-	        }
+	public void onLoot(LootTableLoadEvent event) {
+		
+	    if (LootTableList.CHESTS_DESERT_PYRAMID.equals(event.getName())) {
+	    	
+	        ResourceLocation loc = new ResourceLocation("total_resurrection", "chests/total_resurrection_desert_pyramid_chest");
+	        LootTable customLootTable = event.getLootTableManager().getLootTableFromLocation(loc);
+	        event.setTable(customLootTable);
 	        
 	    }
 	    
