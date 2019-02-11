bc5a738d0c983b870afcecd544041db1f5f6e1d7
LeyxorCheysen <38832246+LeyxorCheysen@users.noreply.github.com>
2019-02-11 10:54:47 +0200

    Added creative tab

---
+++ src/main/java/com/leyxorcheysen/totalresurrection/creativetabs/TotalResurrectionTab.java
@@ -0,0 +1,23 @@
+package com.leyxorcheysen.totalresurrection.creativetabs;
+
+import com.leyxorcheysen.totalresurrection.init.ItemsInit;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemStack;
+
+public class TotalResurrectionTab extends CreativeTabs {
+
+	public TotalResurrectionTab() {
+		
+		super("total_resurection.name");
+		
+	}
+
+	@Override
+	public ItemStack getTabIconItem() {
+		
+		return new ItemStack(ItemsInit.LIFE_CRYSTAL);
+		
+	}
+
+}
