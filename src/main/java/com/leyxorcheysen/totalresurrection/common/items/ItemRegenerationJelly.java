4e69b68c30350cb3860f398828171a9fbd70c846
LeyxorCheysen <38832246+LeyxorCheysen@users.noreply.github.com>
2019-02-12 15:28:40 +0200

    Added regeneration jelly

---
+++ src/main/java/com/leyxorcheysen/totalresurrection/common/items/ItemRegenerationJelly.java
@@ -0,0 +1,23 @@
+package com.leyxorcheysen.totalresurrection.common.items;
+
+import com.leyxorcheysen.totalresurrection.init.TabsInit;
+
+import net.minecraft.init.MobEffects;
+import net.minecraft.item.ItemFood;
+import net.minecraft.potion.PotionEffect;
+
+public class ItemRegenerationJelly extends ItemFood {
+
+	public ItemRegenerationJelly() {
+		
+		super(8, 8.0F, false);
+		
+		this.setCreativeTab(TabsInit.TOTAL_RESURRECTION);
+		this.setMaxStackSize(64);
+		this.setPotionEffect(new PotionEffect(MobEffects.REGENERATION, 1200, 4), 100.0F);
+		this.setRegistryName("regeneration_jelly");
+		this.setUnlocalizedName("totalresurrection.regeneration_jelly");
+		
+	}
+
+}
