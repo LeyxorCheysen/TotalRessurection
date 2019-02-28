934171e7e7cf69ca0ac9865d4678df15b145ee66
LeyxorCheysen <38832246+LeyxorCheysen@users.noreply.github.com>
2019-02-28 21:38:39 +0200

    Update to 0.3.0

--- src/main/java/com/leyxorcheysen/totalresurrection/TotalResurrection.java
+++ src/main/java/com/leyxorcheysen/totalresurrection/TotalResurrection.java
@@ -10,7 +10,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = "totalresurrection", name = "Total Resurrection", version = "0.2.1", dependencies = "required-after:forge@[14.23.5.2768,)", useMetadata = true, acceptedMinecraftVersions = "[1.12.2]", modLanguage = "java", updateJSON = "https://raw.githubusercontent.com/LeyxorCheysen/TotalRessurection/master/versions.json")
+@Mod(modid = "totalresurrection", name = "Total Resurrection", version = "0.3.0", dependencies = "required-after:forge@[14.23.5.2768,)", useMetadata = true, acceptedMinecraftVersions = "[1.12.2]", modLanguage = "java", updateJSON = "https://raw.githubusercontent.com/LeyxorCheysen/TotalRessurection/master/versions.json")
 public class TotalResurrection {
 	
 	@Instance
